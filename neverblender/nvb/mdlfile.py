2cec3762c98d285f4d60da4dd794e53d7eb95bcd
gyoerkaa <gyoerkaa@outlook.com>
2015-06-06 20:21:53 +0200

    derived classes from dummy

--- neverblender/nvb/mdlfile.py
+++ neverblender/nvb/mdlfile.py
@@ -1,19 +1,25 @@
 import bpy
 import os
 import math
+import collections
 
-import neverblender.nvb.walkmesh
+#import neverblender.nvb.walkmesh
 
 class Mdlfile():
-   
-    def __init__(self, 
-                 filepath = '', 
-                 import_objects = {'GEOMETRY', 'ANIMATION', 'WALKMESH', 'LIGHT', 'SHADING_GROUP'},
-                 one_texture_per_image = True,
-                 use_image_search = False,
-                 import_fading_obj = True,
-                 ):
-                 
+    __debug = True
+    
+    def __init__(self, filepath = None):
+        if filepath:
+           open()
+      
+      
+    def open(self, 
+             filepath = '',
+             import_objects = {'GEOMETRY', 'ANIMATION', 'WALKMESH', 'LIGHT', 'SHADING_GROUP', 'EMITTER'},
+             one_texture_per_image = True,
+             use_image_search      = False,
+             import_fading_obj     = True):
+                  
         self.filepath = os.fsencode(filepath)
         self.filename = os.path.splitext(os.path.basename(filepath))[0]
         self.filedir  = os.path.dirname(filepath)
@@ -22,8 +28,8 @@ class Mdlfile():
         self.import_geometry = True;
         self.import_anims    = True;
         self.import_walkmesh = True;
-        self.import_lights   = True;
-        
+        self.import_light    = True;
+        self.import_emitter  = True;
         # Import options (HOW to import)
         self.image_search = use_image_search
         self.one_texture  = one_texture_per_image
@@ -32,41 +38,60 @@ class Mdlfile():
         # Where to put the imported stuff (additional scenes may be created for
         # animations)
         self.scene = bpy.context.scene
-      
-      
-    def load_mdl():
-        ascii_mdl = read_ascii_mdl()
+        
+        ascii_mdl = read_ascii_mdl(self.filepath)
     
     
-    def read_ascii_mdl():
+    def save(self,
+             filepath = '',
+             export_objects = {'GEOMETRY', 'ANIMATION', 'WALKMESH', 'LIGHT', 'SHADING_GROUP', 'EMITTER'},
+             apply_modifiers = True):
+        
+        self.filepath = os.fsencode(filepath)
+        self.filename = os.path.splitext(os.path.basename(filepath))[0]
+        self.filedir  = os.path.dirname(filepath)
+
+        # Exit edit mode before exporting, so current object states are exported properly
+        if bpy.ops.object.mode_set.poll():
+            bpy.ops.object.mode_set(mode='OBJECT')                
+    
+    
+    def read_ascii_mdl(self, filepath = ''):
         '''
         Opens file in ascii format and puts its
         contents into a list    
         '''
         separated_values = []
     
-        with open(self.filepath, 'r') as mdlfile:
-            for line in mdlfile:
-                # Make sure the current line is not a comment
-                if (l[0] != '#'):
-                
-        # Open file
-        try:
-            file  = open(self.filepath, 'r')
+        with open(filepath, 'r') as file:
+            for line in file:
+                line = line.strip()
+                # Skip empty lines & comments
+                if (line and line[0] != '#'):
+                    separated_values.append(line.split())
+                        
+        return separated_values
+    
+    
+    def parse_modelgeom(self, modelgeom):
+        '''
+        Returns an ordered list of geometry nodes.
+        Nodes are orderered "parent-first", i.e. a child will never come before
+        its parent.   
+        '''
         
-            lines = file.readlines()
-            # Iterate over all lines
-            split_line = []
-            for l in lines[0:len(lines)]:
-                # Make sure the current line is not a comment
-                if (l[0] != '#'):      
-                    # Separate words
-                    split_line = l.split()
-                    # if empty, don't add to list
-                    if (len(split_line) > 0):
-                        separated_values.append(split_line)    
+        geomnodes = collections.OrderedDict()
+        
+        if modegeom is None:
+            return geomnodes
+            
+        for line in modegeom:
+            if line[0] = 'node'
     
-        except IOError:
-            print('WARNING: Could not open file ' + str(self.filepath))
     
-        return separated_values  
+    def parse_geom_node(self, geomnode):
+        pass
+    
+    
+    def parse_anim(self, anim):
+        pass
--- neverblender/nvb/nodes.py
+++ neverblender/nvb/nodes.py
@@ -1,46 +1,85 @@
 
 
-
 class Dummy():
     def __init__(self, name):
-        self.wirecolor   = (0.0, 0.0, 0.0)
-        self.position    = (0.0, 0.0, 0.0)
-        self.orientation = (0.0, 0.0, 0.0)
-        self.scale       = (0.0, 0.0, 0.0)
-        
         self.name   = name
-        self.parent = None
+        self.parent = 'null'
+         
+        self.position    = (0.0, 0.0, 0.0)
+        self.orientation = (0.0, 0.0, 0.0, 0.0)
+        self.scale       = 1.0
         
-     def set_scale(self, scale = (0.0, 0.0, 0.0)):
-        self.scale = pos
+        self.wirecolor   = (0.0, 0.0, 0.0)
         
-    def set_position(self, pos = (0.0, 0.0, 0.0)):
-        self.position = pos
-        
-    def set_orientation(self, ori = (0.0, 0.0, 0.0)):
-        self.orientation = ori
-        
-    def set_wirecolor(self, color = (0.0, 0.0, 0.0)):
-        self.wirecolor = color
-       
     def from_object(self, object):
         pass
         
     def to_ascii():
         lines = []
         return lines
-        
-        
-class Trimesh(Dummy):
-    def __init__(self):
-        pass
- 
- 
-class Danglymesh(Trimesh):
-    def __init__(self):
-        pass
 
+
+class Trimesh(Dummy):
+    def __init__(self, name):
+        Dummy.__init__(self, name)
         
+        self.center   = (0.0, 0.0, 0.0)
+        
+        self.tilefade = True
+        self.render   = True
+        self.shadow   = True
+        self.beaming  = True
+        
+        self.inheritcolor     = 0       
+        self.alpha            = 1.0
+        self.transparencyhint = 0
+    
+        self.selfillumcolor = (0.0, 0.0, 0.0)
+        self.ambient        = (0.0, 0.0, 0.0)
+        self.diffuse        = (0.0, 0.0, 0.0)
+        self.specular       = (0.0, 0.0, 0.0)
+        self.shininess      = 0       
+        self.bitmap         = 'null'
+        self.rotatetexture  = 0
+        
+        self.verts           = []
+        self.faces           = []
+        self.tverts          = []
+
+
+class Danglymesh(Trimesh):
+    def __init__(self, name):
+        Trimesh.__init__(self, name)
+        
+        self.period       = 1.0
+        self.tightness    = 1.0
+        self.displacement = 1.0
+
+
 class Skinmesh(Trimesh):
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        Trimesh.__init__(self, name)
+        
+        self.weights = []
+
+
+class Emitter(Dummy):
+    def __init__(self, name):
+        Dummy.__init__(self, name)
+
+
+class Light(Dummy):
+    def __init__(self, name):
+        Dummy.__init__(self, name)
+        
+        self.radius           = 5.0
+        self.multiplier       = 1
+        self.color            = (0.0, 0.0, 0.0)
+        self.ambientonly      = 1
+        self.ndynamictype     = 0
+        self.isdynamic        = 0
+        self.affectdynamic    = 0
+        self.lightpriority    = 5
+        self.fadinglight      = 1
+        self.flareradius      = 1
+
