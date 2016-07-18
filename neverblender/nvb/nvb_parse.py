9e2618404e7bd1ad324e389860a545c6568ad209
gyoerkaa <gyoerkaa@outlook.com>
2016-07-18 23:16:19 +0200

    better mdl parsing

--- neverblender/nvb/nvb_parse.py
+++ neverblender/nvb/nvb_parse.py
@@ -27,6 +27,9 @@ def anim(asciiBlock, animList):
 
 
 def f1(asciiBlock, floatList):
+    '''
+    Parses a series on floats into a list
+    '''
     #floatList = [float(l[0]) for l in asciiBlock]
     l_float = float
     for line in asciiBlock:
@@ -34,27 +37,45 @@ def f1(asciiBlock, floatList):
 
 
 def f2(asciiBlock, floatList):
+    '''
+    Parses a series on float tuples into a list
+    '''
     #floatList = [(float(l[0]), float(l[1])) for l in asciiBlock]
     l_float = float
     for line in asciiBlock:
-        floatList.append( (l_float(line[0]), l_float(line[1])) )
+        floatList.append( (l_float(line[0]),
+                           l_float(line[1])) )
 
 
 def f3(asciiBlock, floatList):
+    '''
+    Parses a series on float 3-tuples into a list
+    '''
     #floatList = [(float(l[0]), float(l[1]), float(l[2])) for l in asciiBlock]
     l_float = float
     for line in asciiBlock:
-        floatList.append( (l_float(line[0]), l_float(line[1]), l_float(line[2])) )
+        floatList.append( (l_float(line[0]),
+                           l_float(line[1]),
+                           l_float(line[2])) )
 
 
 def f4(asciiBlock, floatList):
+    '''
+    Parses a series on float 4-tuples into a list
+    '''
     #floatList = [(float(l[0]), float(l[1]), float(l[2]), float(l[3])) for l in asciiBlock]
     l_float = float
     for line in asciiBlock:
-        floatList.append( (l_float(line[0]), l_float(line[1]), l_float(line[2]), l_float(line[3])) )
+        floatList.append( (l_float(line[0]),
+                           l_float(line[1]),
+                           l_float(line[2]),
+                           l_float(line[3])) )
 
 
 def f5(asciiBlock, floatList):
+    '''
+    Parses a series on float 5-tuples into a list
+    '''
     #floatList = [(float(l[0]), float(l[1]), float(l[2]), float(l[3]), float(l[4])) for l in asciiBlock]
     l_float = float
     for line in asciiBlock:
