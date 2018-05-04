2d7fb4bf6169677d64c34fd9cc6e74f7b7c47a05
gyoerkaa <gyoerkaa@outlook.com>
2018-05-04 15:27:55 +0200

    changed license to GPL v3

--- neverblender/__init__.py
+++ neverblender/__init__.py
@@ -1,21 +1,22 @@
-# ##### BEGIN GPL LICENSE BLOCK #####
+#    BEGIN GPL LICENSE BLOCK
 #
-# Neverblender. Copyright 2012-2018 Attila Gyoerkoes
+#    Neverblender, adds support for the mdl file format to blender
+#    Copyright (C) 2018 Attila Gyoerkoes
 #
-# Neverblender is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
-# (at your option) any later version.
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
-# Neverblender is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with Neverblender.  If not, see <http://www.gnu.org/licenses/>.
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-# ##### END GPL LICENSE BLOCK #####
+#    END GPL LICENSE BLOCK
 
 """TODO: DOC."""
 
