2cb0e184927c78b862aaa6d08997b952357bcd09
gyoerkaa <gyoerkaa@outlook.com>
2019-08-15 18:32:26 +0200

    Version update

--- neverblender/__init__.py
+++ neverblender/__init__.py
@@ -71,8 +71,8 @@ if 'bpy' in locals():
 bl_info = {
     "name": "Neverblender",
     "author": "Attila Gyoerkoes",
-    'version': (2, 8, 7),
-    "blender": (2, 80, 2),
+    'version': (2, 8, 8),
+    "blender": (2, 81, 0),
     "location": "File > Import-Export, Object Properties",
     "description": "Import, export and edit Aurora mdl format",
     "warning": "",
